be41dc30c434dc6423f9193b490cc2789b8f8878
Md. Shihab Mustafa <shihab.mustafa27@gmail.com>
2021-11-23 16:40:20 +0600

    Initial Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use((cors))
+const port = 4000;
+const vegetables = [
+                    {
+                        id: 1, name: 'Cabbage', color: 'green', price: 100
+                    },
+                    {
+                        id: 2, name: 'Carrot', color: 'red', price: 60
+                    },
+                    {
+                        id: 3, name: 'Tomato', color: 'red', price: 80
+                    },
+]
+app.get('/vegetables', (req, res) => {
+    res.send(vegetables)
+})
+app.get('/vegetables/:id', (req, res) => {
+    console.log(req.params.id);
+})
+app.listen(port, () => {
+    console.log('Server is running at http://localhost:${port}')
+})
+app.get('/', (req, res) => res.send('Welcome to my Vegetable Shop!'))
