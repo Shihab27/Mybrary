cafd61588adc305ba852b0d778d1944884ebdc3e
Md. Shihab Mustafa <shihab.mustafa27@gmail.com>
2021-11-24 19:17:10 +0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,14 +16,23 @@ app.set('layout', 'layouts/layout')
 const { once } = require('nodemon')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, err => {
+    if(err) 
+        console.log(err);
+    } 
+);
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
+
 app.listen(process.env.PORT || 3000)
 
  
