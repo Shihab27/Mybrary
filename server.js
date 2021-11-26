da00e9e42dfaf4500d7e65cc4e6a43d4a1f70506
Md. Shihab Mustafa <shihab.mustafa27@gmail.com>
2021-11-26 10:39:15 +0600

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
-    // require('dotenv').load()    //problem line
+  require('dotenv').config()
 }
- 
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -13,26 +13,17 @@ const authorRouter = require('./routes/authors')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-const { once } = require('nodemon')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true })
-mongoose.connect(process.env.MONGODB_URI, err => {
-    if(err) 
-        console.log(err);
-    } 
-);
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })  //connects to server on web
+const db = mongoose.connection  
+db.on('error', error => console.error(error)) //if we run into error we print it on console......
+db.once('open', () => console.log('Connected to Mongoose')) //only runs once
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 
-
 app.listen(process.env.PORT || 3000)
-
- 
